db565519e4ae379eb96a8384c2839d2f615fb3ea
BazookaOrangutan <104569338+BazookaOrangutan@users.noreply.github.com>
2023-06-29 00:13:10 +0300

    commit3

--- Tetris/js/script.js
+++ Tetris/js/script.js
@@ -50,7 +50,9 @@ context.canvas.width = COLS * BLOCK_SIZE;
 context.canvas.height = ROWS * BLOCK_SIZE;
 const canvasNext = document.getElementById('next');
 const ctxNext = canvasNext.getContext('2d');
-
+const linesDiv = document.querySelector('.lines');
+const scoreDiv = document.querySelector('.score');
+const levelDiv = document.querySelector('.level');
 
 for (let x = 0; x < COLS; x++) {
     for (let y = 0; y < ROWS; y++) {
@@ -107,11 +109,13 @@ const colors = {
 };
 
 const grid = 30;
-// const blockList = ['I', 'O', 'T', 'S', 'Z', 'J', 'L'];
 
 let tetrominoSequence = [];
 
-// const nextList = [];
+const nextSequance = [];
+
+
+
 // for (let i = 0; i < 3; i++) {
 //     nextList.push(sequence[randomType()]);
 // }
@@ -129,9 +133,10 @@ function clear() {
 clear();
 
 
-// let count = 0;
 
 let tetromino = getNextTetromino();
+
+let nextTetromino = getNextTetromino();
 
 let rAF = null;
 
@@ -141,13 +146,7 @@ let animFlag = false
 
 
 
-// function randomType() {
-//     return Math.floor(Math.random() * 8);
-// }
 
-// function pushBlockToNext(blockList) {
-
-// }
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
@@ -168,6 +167,11 @@ function generateSequence() {
         tetrominoSequence.push(name);
     }
 }
+
+// function generateNextSequance(){
+    
+// }
+
 
 function getNextTetromino() {
     if (tetrominoSequence.length === 0) {
@@ -226,7 +230,8 @@ function placeTetromino() {
     for (let row = 0; row < tetromino.matrix.length; row++) {
         for (let col = 0; col < tetromino.matrix[row].length; col++) {
             if (tetromino.matrix[row][col]) {
-
+                console.log(tetromino.row);
+                console.log(tetromino.col);
                 // если край фигуры после установки вылезает за границы поля, то игра закончилась
                 if (tetromino.row + row < 0) {
                     return showGameOver();
@@ -237,10 +242,19 @@ function placeTetromino() {
         }
     }
     // проверяем, чтобы заполненные ряды очистились снизу вверх
+    let lineCount = 0;
     for (let row = playfield.length - 1; row >= 0;) {
         // если ряд заполнен
         if (playfield[row].every(cell => !!cell)) {
-
+            lines++;
+            lineCount++;
+            linesDiv.textContent = 'Lines: ' + lines;
+            lines % 5 == 0 ? level++ : level += 0;
+            if (lines % 5 === 0){
+                level++;
+                speed += 0.5;
+            }
+            levelDiv.textContent = 'Level: ' + level;
             // очищаем его и опускаем всё вниз на одну клетку
             for (let r = row; r >= 0; r--) {
                 for (let c = 0; c < playfield[r].length; c++) {
@@ -253,8 +267,29 @@ function placeTetromino() {
             row--;
         }
     }
+
+    switch (lineCount) {
+        case 1:
+            score += 100;
+            scoreDiv.textContent = 'Score: ' + score;
+            break;
+        case 2:
+            score += 300;
+            scoreDiv.textContent = 'Score: ' + score;
+            break;
+        case 3:
+            score += 700;
+            scoreDiv.textContent = 'Score: ' + score;
+            break;
+        case 4:
+            score += 1500;
+            scoreDiv.textContent = 'Score: ' + score;
+            break;
+    }
+
     // получаем следующую фигуру
     tetromino = getNextTetromino();
+    console.log(playfield);
 }
 
 // показываем надпись Game Over
@@ -275,11 +310,12 @@ function showGameOver() {
     context.textAlign = 'center';
     context.textBaseline = 'middle';
     context.fillText('GAME OVER!', canvas.width / 2, canvas.height / 2);
+
 }
 
 let pauseFlag = false
 
-function showPause(){
+function showPause() {
     cancelAnimationFrame(rAF);
     pauseFlag = true;
     context.fillStyle = 'black';
@@ -294,10 +330,9 @@ function showPause(){
     context.fillText('PAUSE', canvas.width / 2, canvas.height / 2);
 }
 
-function removePause(){
+function removePause() {
     rAF = requestAnimationFrame(loop);
     pauseFlag = false;
-
 }
 
 // главный цикл игры
@@ -331,7 +366,8 @@ function loop() {
     if (tetromino) {
 
         // фигура сдвигается вниз каждые 35 кадров
-        if (++count > 35) {
+        count += speed
+        if (count > 35) {
             tetromino.row++;
             count = 0;
 
@@ -359,76 +395,92 @@ function loop() {
 }
 
 // следим за нажатиями на клавиши
-document.addEventListener('keydown', function (e) {
+document.addEventListener('keydown', (e) => {
     // если игра закончилась — сразу выходим
     if (gameOver) return;
 
-    // стрелки влево и вправо
-    if (e.which === 37 || e.which === 39) {
-        const col = e.which === 37
-            // если влево, то уменьшаем индекс в столбце, если вправо — увеличиваем
-            ? tetromino.col - 1
-            : tetromino.col + 1;
+    if (pauseFlag === false) {
+        // стрелки влево и вправо
+        if (e.which === 37 || e.which === 39) {
+            const col = e.which === 37
+                // если влево, то уменьшаем индекс в столбце, если вправо — увеличиваем
+                ? tetromino.col - 1
+                : tetromino.col + 1;
 
-        // если так ходить можно, то запоминаем текущее положение 
-        if (isValidMove(tetromino.matrix, tetromino.row, col)) {
-            tetromino.col = col;
+            // если так ходить можно, то запоминаем текущее положение 
+            if (isValidMove(tetromino.matrix, tetromino.row, col)) {
+                tetromino.col = col;
+            }
         }
-    }
 
-    // стрелка вверх — поворот
-    if (e.which === 38) {
-        // поворачиваем фигуру на 90 градусов
-        const matrix = rotate(tetromino.matrix);
-        // если так ходить можно — запоминаем
-        if (isValidMove(matrix, tetromino.row, tetromino.col)) {
-            tetromino.matrix = matrix;
+        // стрелка вверх — поворот
+        if (e.which === 38) {
+            // поворачиваем фигуру на 90 градусов
+            const matrix = rotate(tetromino.matrix);
+            // если так ходить можно — запоминаем
+            if (isValidMove(matrix, tetromino.row, tetromino.col)) {
+                tetromino.matrix = matrix;
+            }
         }
-    }
 
-    // стрелка вниз — ускорить падение
-    if (e.which === 40) {
-        // смещаем фигуру на строку вниз
-        const row = tetromino.row + 1;
-        // если опускаться больше некуда — запоминаем новое положение
-        if (!isValidMove(tetromino.matrix, row, tetromino.col)) {
+        // стрелка вниз — ускорить падение
+        if (e.which === 40) {
+            // смещаем фигуру на строку вниз
+            const row = tetromino.row + 1;
+            score++;
+            scoreDiv.textContent = 'Score: ' + score;
+            // если опускаться больше некуда — запоминаем новое положение
+            if (!isValidMove(tetromino.matrix, row, tetromino.col)) {
+                tetromino.row = row - 1;
+                // ставим на место и смотрим на заполненные ряды
+                placeTetromino();
+                return;
+            }
+            // запоминаем строку, куда стала фигура
+            tetromino.row = row;
+        }
+        if (e.which === 32) {
+            let row = tetromino.row
+            while (isValidMove(tetromino.matrix, row, tetromino.col)) {
+                row++;
+                score += 2;
+                scoreDiv.textContent = 'Score: ' + score;
+            }
             tetromino.row = row - 1;
-            // ставим на место и смотрим на заполненные ряды
             placeTetromino();
             return;
+
         }
-        // запоминаем строку, куда стала фигура
-        tetromino.row = row;
+
     }
-    // if (e.which === 32) {
-    //     while (isValidMove(tetromino.matrix, row, tetromino.col)) {
-    //         const row = tetromino.row + 1;
-    //     }
-    //     placeTetromino();
-    //     return;
-    // }
-    if ((e.which === 80 || e.which === 27) && animFlag === true){
-        if(pauseFlag) removePause(); 
+    if ((e.which === 80 || e.which === 27) && animFlag === true) {
+        if (pauseFlag) removePause();
         else showPause();
     }
 });
 
-// старт игры
 function play() {
     clear();
-    if (!animFlag) {
+    if (!animFlag || pauseFlag) {
         rAF = requestAnimationFrame(loop);
         animFlag = true;
+        pauseFlag = false;
     }
     tetromino = getNextTetromino();
     count = 0;
     gameOver = false;
     pauseFlag = false;
-
+    lines = 0;
+    level = 0;
+    score = 0;
+    speed = 1;
+    linesDiv.textContent = 'Lines: ' + lines;
+    levelDiv.textContent = 'Level: ' + level;
+    scoreDiv.textContent = 'Score: ' + score;
 }
 
 
-
+console.log(playfield);
 
 
 
